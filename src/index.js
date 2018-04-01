52060c13b79afaa8580db601dc88b2efcf8c43ff
Jaemin Han <jaemin.han@nyu.edu>
2018-04-01 16:08:44 -0400

    initial setup

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ToggleButton from './components/ToggleButton';
+
+ReactDOM.render(
+  <ToggleButton />,
+  document.getElementById('root')
+)
