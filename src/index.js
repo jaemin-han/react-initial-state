4027a410e9e08ea4c9acac5b0904f5ff9fb68b5b
Jaemin Han <jaemin.han@nyu.edu>
2018-04-01 16:16:05 -0400

    example of bad state props method

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import ToggleButton from './components/ToggleButton';
+import Address from './components/Address';
 
 ReactDOM.render(
-  <ToggleButton />,
+  // <ToggleButton />,
+  <Address
+    street="Santa Monica Blvd."
+    city="Santa Monica"
+  />,
   document.getElementById('root')
 )
